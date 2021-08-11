6f6bac230c752aa3abb91147af868355345007aa
Justyna <zadora990605@gmail.com>
2021-08-11 12:18:18 +0200

    Authentication working

--- src/main/java/com/gymmanagement/gymmanagement/GymManagementApplication.java
+++ src/main/java/com/gymmanagement/gymmanagement/GymManagementApplication.java
@@ -2,12 +2,25 @@ package com.gymmanagement.gymmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GymManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GymManagementApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 
 }
